0671b9c6f9d94f61d644dbad62bae57d610ced47
Emmanuel Oyiboke <emmanueloyiboke@gmail.com>
2023-03-13 18:21:26 +0100

    update

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,28 +1,28 @@
-import Link from 'next/link';
-import styles from './style.module.css';
-import Image from 'next/image';
+import Link from "next/link";
+import styles from "./style.module.css";
+import Image from "next/image";
 
-export default function Navbar() {
-      return (
-            <div className={styles.navbar}>
-                  <div className={styles.logo}>
-                        <Image src="/logo.svg" width={43} height={43} alt="logo" />
-                        <span className={styles.logoText}>LINKED DAO</span>
-                  </div>
-                  <div className={styles.navLinks}>
-                        <ul>
-                              <li>
-                                    <Link href="#" className={styles.active}>Home</Link>
-                              </li>
-                              <li>
-                                    <Link href="#">About Us</Link>
-                              </li>
-                              <li>
-                                    <Link href="#">Contact Us</Link>
-                              </li>
-                        </ul>
-                  </div>
-                  <button>Join Discord</button>
-            </div>
-      )
+export default function Navbar({ active, setActive }) {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.logo}>
+        <Image src="/logo.svg" width={43} height={43} alt="logo" />
+        <span className={styles.logoText}>LINKED DAO</span>
+      </div>
+      <div className={`${styles.navLinks} ${active && styles.active}`}>
+        <Link className={styles.active} href="/">
+          Home
+        </Link>
+        <Link href="/">About Us</Link>
+        <Link href="/">Contact Us</Link>
+      </div>
+      <button className={`${styles.btn} ${active && styles.active}`}>
+        Join Discord
+      </button>
+      <button
+        className={styles.burger}
+        onClick={() => setActive(!active)}
+      ></button>
+    </div>
+  );
 }
