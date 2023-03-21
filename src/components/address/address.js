e61494bc1708a3531025e8d748c2459056ec05e2
Emmanuel Oyiboke <emmanueloyiboke@gmail.com>
2023-03-21 15:27:58 +0100

    update

--- src/components/address/address.js
+++ src/components/address/address.js
@@ -6,7 +6,7 @@ export default function Address() {
     <>
       <div className={styles.address}>
         <div className={styles.flex}>
-          <Image src="/link.svg" width={32} height={32} alt="" />
+          <Image src="/avatar.svg" width={32} height={32} alt="" />
           <p>Oxwi14 hkb3hvd8b2chjshdvbhwiv23fd</p>
         </div>
         <Image width={32} height={32} src="/link.svg" alt="" />
